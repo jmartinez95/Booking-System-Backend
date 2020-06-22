53d4857a9747588fb36bf85c0965275fe27bd97f
Jacob Martinez <jacob@buildthis.com>
2020-06-22 16:37:27 -0500

    Added procfile

--- app.js
+++ app.js
@@ -5,11 +5,62 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/api', graphQlHttp({
-    schema: null,
-    rootValue: {}
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+        `),
+    rootValue: {
+        events: () => {
+            return events;
+        },
+        createEvent: (args) => {
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: new Date().toISOString()
+            }
+            events.push(event);
+            return event;
+        }
+    },
+    graphiql: true
 }));
 
-app.listen(3000);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port);
