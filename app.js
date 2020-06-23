395a38e44e02cfe3727cfd25a7eb7a6d7fb4c8be
Jacob Martinez <jacob@buildthis.com>
2020-06-23 13:18:55 -0500

    Added async and pulled out files

--- app.js
+++ app.js
@@ -1,60 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const graphQlHttp = require('express-graphql')
-const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
 
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
 
+
+
 app.use('/api', graphQlHttp({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
-        
-        type RootQuery {
-            events: [Event!]!
-        }
-
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-        `),
-    rootValue: {
-        events: () => {
-            return events;
-        },
-        createEvent: (args) => {
-            const event = {
-                _id: Math.random().toString(),
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date().toISOString()
-            }
-            events.push(event);
-            return event;
-        }
-    },
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true
 }));
 
@@ -63,4 +23,9 @@ if (port == null || port == "") {
   port = 8000;
 }
 
-app.listen(port);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ipwcs.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`).then(() => {
+    app.listen(port);
+}).catch(err => {
+    console.log(err);
+})
+
